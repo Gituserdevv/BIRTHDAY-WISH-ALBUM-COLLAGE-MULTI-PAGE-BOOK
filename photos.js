d3267ded45ae2da17f55b9ea00f3ac3a807308a6
Gituserdevv <gitdevelopinguser@protonmail.com>
2026-08-11 17:40:28 +0530

    Create photos.js

---
+++ photos.js
@@ -0,0 +1,11 @@
+// ==========================================
+// 📸 ALBUM PHOTOS CONFIGURATION FILE
+// Bas yahan apne Image Links paste karein
+// ==========================================
+
+const MY_CUSTOM_PHOTOS = [
+    "https://images.unsplash.com/photo-1517841905240-472988babdf9?w=500", // Photo 1 (Cover & Scratch)
+    "https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=500", // Photo 2 (Collage 1)
+    "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=500", // Photo 3 (Collage 2)
+    "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=500"  // Photo 4 (Collage 3)
+];
